59eae5f9c09730d57074f305f2603280f44173a8
harlyGuerrero <harlydavinsonguerrero@gmail.com>
2026-06-03 13:09:10 -0500

    03-06-2026 - 13:09

--- front-end/src/features/auth/components/LoginForm.tsx
+++ front-end/src/features/auth/components/LoginForm.tsx
@@ -108,7 +108,7 @@ export function LoginForm({ ...props }: React.ComponentProps<"div">) {
                 </div>
 
                 <p className="mt-7 max-w-[285px] text-[15px] leading-7 text-white/95">
-                  Control integral de activos fijos e infraestructura
+                  Control SOlecita integral de activos fijos e infraestructura
                   institucional con los más altos estándares de seguridad y
                   eficiencia gubernamental.
                 </p>
