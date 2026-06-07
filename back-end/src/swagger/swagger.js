1727bc2ed809fb628f53809d0ded048767b7ea5a
Carlos Herrera <cghv21304@gmail.com>
2026-06-07 10:32:51 -0500

    retorno a lo de antes

--- back-end/src/swagger/swagger.js
+++ back-end/src/swagger/swagger.js
@@ -10,17 +10,9 @@ const options = {
         },
         servers: [
             {
-                url: 'http://192.168.18.15:3000'
+                url: 'http://localhost::3000'
             }
         ],
-        /**
-         * Se hizo el respectivo cambio para hacer pruebas de servidor casero
-         * servers: [
-         *             {
-         *                 url: 'http://localhost:3000'
-         *             }
-         * ],
-         */
         components: {
             securitySchemes: {
                 bearerAuth: {
